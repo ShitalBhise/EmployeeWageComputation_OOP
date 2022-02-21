3c4a9d70da6e70c53279ae90affce64f91c02a45
ShitalBhise <bhiseshital27059@gmail.com>
2022-02-21 23:28:06 +0530

    Added:Welcome message

---
+++ src/com/java/EmpWageCmp_OOP.java
@@ -0,0 +1,9 @@
+package com.java;
+
+public class EmpWageCmp_OOP {
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Employee Wage check Computation by OOP's Concept");
+
+	}
+}
